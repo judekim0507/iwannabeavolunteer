e80d4116885d58c6cdb53595bbaf0536de1c77bb
Jude Kim <75868541+judekim0507@users.noreply.github.com>
2025-11-11 23:12:13 -0800

    fix: version update

--- src/lib/appVersion.ts
+++ src/lib/appVersion.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "1.1.2";
+export const APP_VERSION = "1.1.3";
 
 /*
 Simple Version History for me (or whoever reading this on github)
@@ -8,4 +8,5 @@ RELEASE
 - v1.1.0 - Added multi-council support with multi-admin
 - v1.1.1 - Fixed bug with council creation and admin page refactor
 - v1.1.2 - Admin page not loading issue addressed; version tracking added; duplication prevention added; loading order fixed (smaller -> larger); council memory system; CURRENT
+- v1.1.3 - PWA Support
 */
